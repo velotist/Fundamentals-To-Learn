d8d6008641902b7223d08443156ea1999cf593e2
whonowhere <whut@nowhere.org>
2020-10-30 10:57:23 +0100

    Outsource properties to Interface
    [feature]

--- InterfaceExercises/Program.cs
+++ InterfaceExercises/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace InterfaceExercises
 {
     interface IDoSomething
     {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
         string GetName();
         int GetAge();
         int Multiply(int a, int b);
@@ -14,25 +18,32 @@ namespace InterfaceExercises
     {
         static void Main(string[] args)
         {
-            DoIt ageIs37 = new DoIt
-            {
-                Age = 37
-            };
+            DoIt defaultType = new DoIt();
 
-            ageIs37.Print();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine(ageIs37.GetName());
-            Console.WriteLine(ageIs37.GetAge());
-            Console.WriteLine(ageIs37.Multiply(3,4));
+            Console.WriteLine(defaultType.Age);
+            Console.WriteLine(defaultType.GetName());
+            Console.WriteLine(defaultType.GetAge());
+            Console.WriteLine(defaultType.Multiply(3,4));
+
+            DoIt ageIs48 = new DoIt
+            {
+                Age = 48
+            };
+            Console.WriteLine(defaultType.Equals(ageIs48));
+
             Console.ReadKey();
         }
     }
 
-    class DoIt : IDoSomething
+    class DoIt : IDoSomething, IEquatable<DoIt>
     {
-        public int Age { get; set; }
-        public string Name { get; set; }
+        public int Age { get; set; } = 1;
+        public string Name { get; set; } = "noname";
+
+        public bool Equals([AllowNull] DoIt other)
+        {
+            return (this.Age, this.Name) == (other.Age, other.Name);
+        }
 
         public int GetAge()
         {
