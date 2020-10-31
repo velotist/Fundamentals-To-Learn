709cd7b87bec5f9c93b1897950d283c2a493971f
velotist <velospam@posteo.de>
2020-10-31 21:06:14 +0100

    Add tests for class DoSomethin
    [feature]

---
+++ DoSomethinTests/DoSomethinTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DoSomethinTests
+{
+    public class DoSomethinTest
+    {
+        [Fact]
+        public void GetAge_InputString_ReturnZero()
+        {
+            
+        }
+    }
+}
