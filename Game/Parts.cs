6f9f7d38e6e576439c4fc9bcf63850a18dabebeb
velotist <velospam@posteo.de>
2020-11-02 20:29:57 +0100

    Add new class Game
    [feature]

---
+++ Game/Parts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    struct Rectangle
+    {
+        public Rectangle(int width, int length)
+        {
+            this.Width = width;
+            this.Length = length;
+        }
+
+        public int Width { get; }
+
+        public int Length { get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectangle = new Rectangle(100,200);
+
+            Console.WriteLine("Width: {0}", rectangle.Width);
+            Console.WriteLine("Length: {0}", rectangle.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
